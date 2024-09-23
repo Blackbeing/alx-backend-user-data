35cec7109b60c4a3bb27a76f995eb19654ae26d1
Blackbeing <jonnieey55@gmail.com>
2024-09-23 00:23:12 +0000

    feat Excluded path logic

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,11 @@ from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path and not path.endswith("/"):
+            path = f"{path}/"
+
+        if not excluded_paths or path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
